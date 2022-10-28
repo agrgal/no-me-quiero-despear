3094ddff0d734d70f651e6e068a2f9348b665d50
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2022-10-28 06:20:05 +0200

    Depurado

--- main.ts
+++ main.ts
@@ -1,16 +1,11 @@
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 30)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 60)
-        maqueen.motorStop(maqueen.Motors.M2)
-    } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 60)
-        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
     } else {
         maqueen.motorStop(maqueen.Motors.All)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 60)
+        basic.pause(200)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 200)
         basic.pause(500)
     }
 })
